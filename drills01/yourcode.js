eb3d39ed3203ce71ec1ed025a35b33c41a770d48
Lena Porina <porinalena@gmail.com>
2019-09-14 12:54:56 -0700

    Made changes to array in sumArray function

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,9 +1,8 @@
 
 function sumArray(array) {
-  var numbersArray = array;
   var sum = 0;
-  for (var i = 0; i < numbersArray.length; i++) {
-    sum += numbersArray[i];
+  for (var i = 0; i < array.length; i++) {
+    sum += array[i];
   }
   return sum;
 }
